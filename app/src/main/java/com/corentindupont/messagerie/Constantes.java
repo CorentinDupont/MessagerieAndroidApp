2476c6d702db7fcd5fccc2fe0e630efa4aa34f34
CorentinDupont <dupont.corentin44@gmail.com>
2018-03-09 11:25:18 +0100

    Save Account Edit Text Datas

---
+++ app/src/main/java/com/corentindupont/messagerie/Constantes.java
@@ -0,0 +1,11 @@
+package com.corentindupont.messagerie;
+
+/**
+ * Created by Corentin on 09/03/2018.
+ */
+
+public class Constantes {
+    public final static String ACCOUNT_EMAIL = "accountEmail";
+    public final static String ACCOUNT_NAME = "accountName";
+    public final static String ACCOUNT_PASSWORD = "accountPassword";
+}
--- app/src/main/java/com/corentindupont/messagerie/Fragment/AccountFragment.java
+++ app/src/main/java/com/corentindupont/messagerie/Fragment/AccountFragment.java
@@ -2,11 +2,14 @@ package com.corentindupont.messagerie.Fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.corentindupont.messagerie.Constantes;
 import com.corentindupont.messagerie.R;
 
 
@@ -15,6 +18,9 @@ import com.corentindupont.messagerie.R;
  */
 public class AccountFragment extends Fragment {
 
+    EditText mailEditText;
+    EditText nameEditText;
+    EditText passwordEditText;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -26,6 +32,32 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //All Account Edit Text
+        mailEditText = (EditText) getView().findViewById(R.id.account_email_edit_text);
+        nameEditText = (EditText) getView().findViewById(R.id.account_name_edit_text);
+        passwordEditText = (EditText) getView().findViewById(R.id.account_password_edit_text);
+
+        if(savedInstanceState != null){
+            //Restore account edit text values
+            mailEditText.setText(savedInstanceState.getString(Constantes.ACCOUNT_EMAIL));
+            nameEditText.setText(savedInstanceState.getString(Constantes.ACCOUNT_NAME));
+            passwordEditText.setText(savedInstanceState.getString(Constantes.ACCOUNT_PASSWORD));
+        }
+    }
+
+    //Save Account Edit Text Values
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(Constantes.ACCOUNT_EMAIL, String.valueOf(mailEditText.getText()));
+        outState.putString(Constantes.ACCOUNT_NAME, String.valueOf(nameEditText.getText()));
+        outState.putString(Constantes.ACCOUNT_PASSWORD, String.valueOf(passwordEditText.getText()));
+        super.onSaveInstanceState(outState);
     }
 
 }
